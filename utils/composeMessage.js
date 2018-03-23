db407216e8bddbf9b671cc3152c94b3bf280c13c
Виталий Набережный <naberezhniy@taxi21.ru>
2018-03-23 14:33:37 +0300

    --

--- utils/composeMessage.js
+++ utils/composeMessage.js
@@ -7,8 +7,8 @@ const composeMessage = post => {
     const R = post.title.match(/r/i);
     post.title = _.escape(post.title);
     message = R
-      ? `${post.title}\n<i>${post.topic}</i> | <b>${post.author}</b>\n${post.link}`
-      : `<a href='${post.href}'>${post.title}</a>\n<i>${post.topic}</i> | <b>${post.author}</b>\n${post.link}`;
+      ? `<b>${post.title}</b>\n<i>${post.topic}</i> | ${post.author}\n${post.link}`
+      : `<b>${post.title}</b> <a href='${post.href}'> > пройти по ссылке</a>\n<i>${post.topic}</i> | ${post.author}\n${post.link}`
   } else {
     // .format('DD MMMM YYYY HH:ss')
     message = `${post.title}\n${post.link}`
