69e94f55e76784e41160811ec1d2f4d3dc4a1c7e
Виталий Набережный <naberezhniy@taxi21.ru>
2019-10-22 15:38:07 +0300

    --

--- changeProxy.js
+++ changeProxy.js
@@ -2,11 +2,11 @@ const request = require('request-promise');
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-let doc = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8'));
+var doc = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8'));
 request(doc.common.getProxyUrl)
   .then(function (proxy) {
     proxy = JSON.parse(proxy);
-    var proxyUrl = `${proxy.ip}:${proxy.port}`;
+    var proxyUrl = "http://" + proxy.ip + ":" + proxy.port;
     doc.common.proxy = proxyUrl;
     fs.writeFileSync('./config.yaml', yaml.safeDump(doc));
   })
